3378199f61e0bd684746d885a5599b56c9eeca34
German Fica <germafica@gmail.com>
2021-05-26 10:09:43 -0300

    Using Spring Container

---
+++ core/src/main/java/com/germanfica/NumberGenerator.java
@@ -0,0 +1,6 @@
+package com.germanfica;
+
+public interface NumberGenerator {
+    int next();
+    int getMaxNumber();
+}
---
+++ core/src/main/java/com/germanfica/NumberGeneratorImpl.java
@@ -0,0 +1,20 @@
+package com.germanfica;
+
+import java.util.Random;
+
+public class NumberGeneratorImpl implements NumberGenerator {
+    // == fields ==
+    private final Random random = new Random();
+    private int maxNumber = 100;
+
+    // == public methods ==
+    @Override
+    public int next() {
+        return random.nextInt(maxNumber);
+    }
+
+    @Override
+    public int getMaxNumber() {
+        return maxNumber;
+    }
+}
