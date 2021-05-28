e38b30a85952a689db1a3a6a032c816acb91795d
German Fica <germafica@gmail.com>
2021-05-28 01:22:37 -0300

    Console Module Setup Challenge

--- console/src/main/java/com/germanfica/console/Main.java
+++ console/src/main/java/com/germanfica/console/Main.java
@@ -1,10 +1,13 @@
-package com.germanfica;
+package com.germanfica.console;
 
+import com.germanfica.AppConfig;
+import com.germanfica.Game;
+import com.germanfica.MessageGenerator;
+import com.germanfica.NumberGenerator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     private static final Logger log = LoggerFactory.getLogger(Main.class);
