b6f3938eeeb23627491786956678b68510fed1e3
German Fica <germafica@gmail.com>
2021-05-29 15:34:46 -0300

    Setup Lombok

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Guess The Number Game
 
 I'm following the Java Spring Course by [Tim Buchalka](https://www.udemy.com/course/java-spring-framework-masterclass/).
+
+## Maven commands
+
+- `mvn clean`
+- `mvn clean install`
--- console/src/main/java/com/germanfica/console/Main.java
+++ console/src/main/java/com/germanfica/console/Main.java
@@ -1,13 +1,14 @@
 package com.germanfica.console;
 
 import com.germanfica.config.GameConfig;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+@Slf4j
 public class Main {
-    private static final Logger log = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
         log.info("Guess The Number Game");
