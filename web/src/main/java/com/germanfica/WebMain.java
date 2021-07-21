37d4491e1620bbc14442ab47d13d282149610901
German Fica <germafica@gmail.com>
2021-07-21 15:24:28 -0300

    Spring Boot Web Module
    Keep in mind here that only the core module is shared between the web and console modules.

---
+++ web/src/main/java/com/germanfica/WebMain.java
@@ -0,0 +1,11 @@
+package com.germanfica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMain {
+    public static void main(String[] args) {
+        SpringApplication.run(WebMain.class, args);
+    }
+}
