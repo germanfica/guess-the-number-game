a4d2035f9d26dc5a4f7ebb6fc5d707543acdcf39
German Fica <germafica@gmail.com>
2021-07-21 01:03:59 -0300

    Add Spring Boot to an Existing Project

--- console/src/main/java/com/germanfica/Main.java
+++ console/src/main/java/com/germanfica/Main.java
@@ -1,22 +1,20 @@
-package com.germanfica.console;
+package com.germanfica;
 
 import com.germanfica.config.GameConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @Slf4j
+@SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
         log.info("Guess The Number Game");
-
-        // create context (container)
-        ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(GameConfig.class);
-
-        // close context (container)
-        context.close();
+        SpringApplication.run(Main.class, args);
     }
 }
