cabc777406aadffcf640af491e72f5484f670855
German Fica <germafica@gmail.com>
2021-07-25 18:24:02 -0300

    Locale Change Interceptor

--- web/src/main/java/com/germanfica/config/WebConfig.java
+++ web/src/main/java/com/germanfica/config/WebConfig.java
@@ -2,17 +2,34 @@ package com.germanfica.config;
 
 import com.germanfica.interceptor.RequestInterceptor;
 import com.germanfica.util.ViewNames;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    // == beans methods ==
+    @Bean
+    public LocaleResolver localeResolver () {
+        return new SessionLocaleResolver();
+    }
+
+    // == public methods ==
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         WebMvcConfigurer.super.addInterceptors(registry);
         registry.addInterceptor(new RequestInterceptor()); // Adding interceptor
+
+//        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+//        localeChangeInterceptor.setParamName("lang");
+//        registry.addInterceptor(localeChangeInterceptor);
+        registry.addInterceptor(new LocaleChangeInterceptor()); // The default is http://localhost:8080/?locale=en
     }
 
     @Override
